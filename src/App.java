4abfe0884e48011ab22ac5fecfc4ad34ab01c4bd
maria roberts <mariaroberts15@outlook.com>
2021-06-01 23:27:03 -0400

    fixed some stuff ig

--- src/App.java
+++ src/App.java
@@ -5,20 +5,17 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -206,7 +203,6 @@ public class App extends Application {
             }
             if (e.getCode() == KeyCode.SPACE) {
                 progressInt++;
-                System.out.println(progressInt);
                 dialogueMethod();
             }
         });
